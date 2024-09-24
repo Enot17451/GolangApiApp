c24a9cf23de27a0f913104a1a2e18d9abcd42fa4
user <homeMail@.ru>
2024-09-24 23:44:35 +0500

    home

--- main.go
+++ main.go
@@ -1,13 +1,26 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
+type User struct {
+	name string
+	age  int
+}
+
 func mainPage(response http.ResponseWriter, request *http.Request) {
-	fmt.Println(request.URL)
+	user := User{"Mike", 12}
+	templ, err := template.ParseFiles("html/main.html")
+	if err != nil {
+		return
+	}
+	err = templ.Execute(response, user)
+	if err != nil {
+		return
+	}
 }
 
 func main() {
